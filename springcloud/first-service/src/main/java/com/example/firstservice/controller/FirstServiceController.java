574530ed8e50569477196885cd1a803ab4e772a3
sungjin96 <sungjin5891@gmail.com>
2022-03-26 21:09:44 +0900

    gateweay example

--- springcloud/first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ springcloud/first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,6 +1,7 @@
-package com.example.secondservice.controller;
+package com.example.firstservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by marathoner on 2022/03/26
  */
 @RestController
-@RequestMapping("/")
-public class SecondServiceController {
+@RequestMapping("/first-service")
+public class FirstServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome to the Second Service";
+        return "Welcome to the First Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        System.out.println(header);
+        return "Hello World in First Service.";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there, This is a message from First Service.";
     }
 }
--- springcloud/second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
+++ springcloud/second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
@@ -1,4 +1,30 @@
-package com.example.secondservice.controller;/**
+package com.example.secondservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * Created by marathoner on 2022/03/26
- */public class SecondServiceController {
+ */
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        System.out.println(header);
+        return "Hello World in Second Service.";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there, This is a message from Second Service.";
+    }
 }
