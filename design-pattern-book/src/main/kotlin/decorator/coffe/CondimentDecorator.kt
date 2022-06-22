7f16aa6478a278146e3b836e8ce71367678cdb1d
sungjin96 <sungjin5891@gmail.com>
2022-06-22 20:14:20 +0900

    코드스피츠 프로젝트 생성

---
+++ design-pattern-book/src/main/kotlin/decorator/coffe/CondimentDecorator.kt
@@ -0,0 +1,6 @@
+package decorator.coffe
+
+/**
+ * Created by marathoner on 2022/04/20
+ */class condimentDecorator {
+}
---
+++ design-pattern-book/src/main/kotlin/decorator/coffe/beverages/Mocha.kt
@@ -0,0 +1,6 @@
+package decorator.coffe.beverages
+
+/**
+ * Created by marathoner on 2022/04/20
+ */class Mocha {
+}
