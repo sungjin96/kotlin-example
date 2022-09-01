6afe18030648e59d8a2d35c1e4616876b16eb528
sungjin96 <sungjin5891@gmail.com>
2022-09-01 20:54:58 +0900

    서비스 레이어 테스트

--- junit5-tutorial/src/test/kotlin/com/example/junit5tutorial/service/BookServiceTest.kt
+++ junit5-tutorial/src/test/kotlin/com/example/junit5tutorial/service/BookServiceTest.kt
@@ -1,5 +1,6 @@
 package com.example.junit5tutorial.service
 
+import com.example.junit5tutorial.domain.Book
 import com.example.junit5tutorial.domain.BookRepository
 import com.example.junit5tutorial.util.MailSender
 import com.example.junit5tutorial.web.dto.BookCreateRequestDto
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
 
 /**
  * Created by marathoner on 2022/08/23
@@ -18,10 +20,14 @@ import org.mockito.junit.jupiter.MockitoExtension
 @ExtendWith(MockitoExtension::class)
 internal class BookServiceTest {
 
+    lateinit var a: String
+
     @InjectMocks
     private lateinit var bookService: BookService
+
     @Mock
     private lateinit var bookRepository: BookRepository
+
     @Mock
     private lateinit var mailSender: MailSender
 
@@ -41,4 +47,43 @@ internal class BookServiceTest {
         assertThat(bookDto.title).isEqualTo(newBook.title)
         assertThat(bookDto.author).isEqualTo(newBook.author)
     }
+
+    @Test
+    fun `should 책 목록보기 테스트`() {
+        // stub
+        val books = arrayListOf<Book>(
+            Book(title = "title", author = "author"),
+            Book(title = "title2", author = "author2")
+        )
+        Mockito.`when`(bookRepository.findAll()).thenReturn(books)
+
+        // when
+        val findBooks = bookService.findAll()
+
+        // then
+        assertThat("title").isEqualTo(findBooks[0].title)
+        assertThat("author").isEqualTo(findBooks[0].author)
+        assertThat("title2").isEqualTo(findBooks[1].title)
+        assertThat("author2").isEqualTo(findBooks[1].author)
+
+    }
+
+    @Test
+    fun `should 책 한권보기 테스트`() {
+        // given
+
+        // stub
+        val book: Optional<Book> = Optional.of(Book(title = "title", author = "author"))
+
+        Mockito.`when`(bookRepository.findById(any())).thenReturn(book)
+
+        //when
+        val findBook = bookService.findById(book.get().id)
+
+        // then
+        assertThat(book.get().title).isEqualTo(findBook.title)
+        assertThat(book.get().author).isEqualTo(findBook.author)
+
+    }
+
 }
