c691e947fd4c7d450f1f44f180e1a2a1c4750080
sungjin96 <sungjin5891@gmail.com>
2022-10-24 21:44:43 +0900

    book controller test 추가

--- junit5-tutorial/src/test/kotlin/com/example/junit5tutorial/web/BookApiControllerTest.kt
+++ junit5-tutorial/src/test/kotlin/com/example/junit5tutorial/web/BookApiControllerTest.kt
@@ -1,8 +1,63 @@
 package com.example.junit5tutorial.web
 
-import org.junit.jupiter.api.Assertions.*
+import com.example.junit5tutorial.service.BookService
+import com.example.junit5tutorial.web.dto.BookCreateRequestDto
+import com.example.junit5tutorial.web.dto.BookResponseDto
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.jayway.jsonpath.JsonPath
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
 
 /**
  * Created by marathoner on 2022/08/23
  */
-internal class BookApiControllerTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+internal class BookApiControllerTest {
+
+    @Autowired
+    private lateinit var bookServer: BookService
+
+    @Autowired
+    private lateinit var rt: TestRestTemplate
+
+    @Autowired
+    private lateinit var om: ObjectMapper
+
+    @Test
+    fun `should book save test`() {
+        // given
+        val bookCreateRequestDto = BookCreateRequestDto(
+            title = "Spring",
+            author = "Jang",
+        )
+
+        val body = om.writeValueAsString(bookCreateRequestDto)
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val httpEntity = HttpEntity(body, headers);
+
+        // when
+        val response = rt.exchange(
+            "/api/v1/book",
+            HttpMethod.POST,
+            httpEntity,
+            String::class.java
+        )
+
+        // then
+        val dc = JsonPath.parse(response.body)
+        val title = dc.read<String>("$.body.title")
+        val author = dc.read<String>("$.body.author")
+
+        assertThat(bookCreateRequestDto.title).isEqualTo(title)
+        assertThat(bookCreateRequestDto.author).isEqualTo(author)
+
+    }
+}
